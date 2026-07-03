6676d3ed5b89bc1802ddf75999d28fb6fa096304
developer2propy <developer2propy@gmail.com>
2026-07-03 18:54:58 +0530

    Domain changed

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  base: "https://expoadmin.terraterri.com/",
-//   base: '/',
-  plugins: [
-    react({
-      // fastRefresh: false
-    })
-  ]
+  base: '/',
+  plugins: [react()]
 });
